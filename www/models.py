217223179bf0a7d56cd45e4e5ea4a19a0880788e
Etiboy <etiboydemas@gmail.com>
2016-10-08 14:11:56 +0530

    changes to 3 files accepted

--- www/models.py
+++ www/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 '''
-This options basically helps track number of days
+This options basically helps track number of days!
 WEEK_DAYS = (
     (1, 'Monday'),
     (2, 'Tuesday'),
--- www/views.py
+++ www/views.py
@@ -3,5 +3,5 @@ from .models import Restaurant
 
 # Create your views here.
 def index(request):
-	#returning all restaurant object in a restaurants context, which can be accessed in the index.html file.
+	#returning all restaurant object in a restaurants context, which can be accessed in the index.html file. Yes o.
 	return render(request,'index.html',{'restaurants': Restaurant.objects.all()})
