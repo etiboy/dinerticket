f11633fe82b29ef5f329f9bcec115756d6e81403
Etiboy <etiboydemas@gmail.com>
2016-10-11 01:42:44 +0530

    added detailed pages for restaurants

--- www/urls.py
+++ www/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'www'
 urlpatterns = [
-	url(r'^$',views.index)
+	url(r'^$',views.IndexView.as_view(),name='index'),
+	url(r'^restaurant/(?P<pk>[0-9]+)/$',views.RestaurantView.as_view(),name='restaurant')
 ]
--- www/views.py
+++ www/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from .models import Restaurant
-
+from django.views import generic
 # Create your views here.
-def index(request):
+#def index(request):
 	#returning all restaurant object in a restaurants context, which can be accessed in the index.html file. Yes o.
-	return render(request,'index.html',{'restaurants': Restaurant.objects.all()})
+#	return render(request,'index.html',{'restaurants': Restaurant.objects.all()})
+
+class IndexView(generic.ListView):
+	template_name = "index.html"
+	context_object_name = "restaurants"
+
+	def get_queryset(self):
+		return Restaurant.objects.all()
+
+class RestaurantView(generic.DetailView):
+	model = Restaurant
+	template_name = "restaurant.html"
